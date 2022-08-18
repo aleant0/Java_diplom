5ca7a086cbefe01927baa45341401f857d045e11
Aleant <alalant50@gmail.com>
2022-08-18 20:31:35 +0300

    Tests added

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -15,4 +15,78 @@ public class GameStoreTest {
     }
 
     // другие ваши тесты
+
+    @Test
+    public void shouldNotFindNonExistentGame() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = new Game("Нетология Баттл Онлайн 2", "Аркады", store);
+
+        assertFalse(store.containsGame(game2));
+    }
+
+    @Test
+    public void shouldAddPlayTimeFirstTime() {
+
+        GameStore store = new GameStore();
+
+        store.addPlayTime("Petr",  5);
+
+        int expected = 5;
+        int actual = store.getSumPlayedTime();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddPlayTimeSecondTime() {
+
+        GameStore store = new GameStore();
+
+        store.addPlayTime("Petr",  5);
+        store.addPlayTime("Petr",  10);
+
+        int expected = 15;
+        int actual = store.getSumPlayedTime();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetMostPlayer() {
+
+        GameStore store = new GameStore();
+
+        store.addPlayTime("Petr",  5);
+        store.addPlayTime("Oleg",  10);
+
+        String expected = "Oleg";
+        String actual = store.getMostPlayer();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetMostPlayerIfNoPlayers() {
+
+        GameStore store = new GameStore();
+
+        store.addPlayTime("Petr",  5);
+        store.addPlayTime("Oleg",  5);
+
+        String expected = "Oleg";
+        String actual = store.getMostPlayer();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetMostPlayerIfHoursEqual() {
+
+        GameStore store = new GameStore();
+
+        assertEquals(null, store.getMostPlayer());
+    }
+
 }
